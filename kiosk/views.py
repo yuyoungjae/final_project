9b2631d5079a8ea67b524230dac95161ac4c24a3
yanghs6 <yanghs632@gmail.com>
2022-06-26 17:04:44 +0900

    [feature] category switch by url parameter

--- kiosk/views.py
+++ kiosk/views.py
@@ -2,16 +2,30 @@ from django.shortcuts import render
 from django.views import View
 from django_request_mapping import request_mapping
 
+from kiosk.models import Menu
+
 
 @request_mapping("")
 class MyView(View):
-
+    orderlist = list()
+    
+    # def __init__(self):
+        # orderlist = list()
+    
     @request_mapping("/", method="get")
-    def index(self, request):
-        from kiosk.models import Menu
-        objs = Menu.objects.all()
+    def index_init(self, request):
+        return self.index(request)
+    
+    
+    # path("", views.cocktails),
+    @request_mapping("<int:id>/", method="get")
+    def index(self, request, id=6):
+        context = dict()
 
-        context = {
-            'objs': objs
-        }
+        objs = Menu.objects.values("name", "price", "category_id")
+
+        context['objs'] = objs
+        context['category_id'] = id if 1 <=id <=5 else 6
+            
+            
         return render(request, 'index.html', context)
